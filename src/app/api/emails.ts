f0100c3e0b466ccf38ce2236b67da23cf4925a1a
farface <149008522+farface@users.noreply.github.com>
2023-10-27 10:49:33 -0400

    Update emails.ts

--- src/app/api/emails.ts
+++ src/app/api/emails.ts
@@ -1,5 +1,5 @@
 import axios from "axios";
-const BASE_URL = process.env.NEXT_PUBLIC_BACKEND_URL;
+const BASE_URL = https://abskr4mzpi.us-east-2.awsapprunner.com;
 
 export default axios.create({
   baseURL: BASE_URL,
